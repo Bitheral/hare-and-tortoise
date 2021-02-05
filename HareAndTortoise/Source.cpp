dc2754d821be2a80dac494da92cfc00f48bc56ee
Bitheral <me@bitheral.net>
2021-02-05 09:56:48 +0000

    Finished

--- HareAndTortoise/Source.cpp
+++ HareAndTortoise/Source.cpp
@@ -1,144 +1,437 @@
-#include <iostream>
-#include "console.h"
-#include <Windows.h>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <regex>
 #include <cstdlib>
 #include <ctime>
-#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "console.h"
 
 using namespace std;
 
-void drawLineH(int, int, int, char);
-void drawAnimal(int, int, char);
+class Track {
+private:
+	int position[2] = { 0,0 }, width = 0;
+	double speed = 0;
+
+public:
+	Track(string filename) {
+
+		// Open Track config file
+		ifstream inFile(filename);
+
+
+		// Check if the file exists
+		if (inFile) {
+
+			// File exists - Extract data
+			string line;
+			int lineCount = 1;
+			while (getline(inFile, line))
+			{
+				/* Set data depending on line number
+				* 
+				* 1. X Position
+				* 2. Y Position
+				* 3. Track width
+				* 4. Simulation speed
+				*
+				*/
+
+				position[lineCount - 1] = stoi(line);
+				switch (lineCount) {
+				case 3:
+					width = stoi(line);
+					break;
+				case 4:
+					speed = stod(line);
+					break;
+				}
+				lineCount++;
+			}
+		}
+		else {
+
+			// File doesn't exist - Throw an error with red text
+			// then quit the program after 5 seconds.
+			Console::setColour(Console::RED, Console::WHITE);
+			Console::setCursorPosition(2,0);
+			cout << "Error trying to read file." << endl;
+			Console::setCursorPosition(3, 0);
+			cout << "File `" << filename << "` may not exist" << endl;
+			Console::setCursorPosition(5, 0);
+			Console::setColour(Console::WHITE, Console::BLACK);
+			cout << "`" << filename << "` requires the following format:" << endl;
+			cout << "X Position - How far along the X Axis the Track is in the console window" << endl;
+			cout << "Y Position - How far along the Y Axis the Track is in the console window" << endl;
+			cout << "Width - How wide the track is along the X Axis" << endl;
+			cout << "Simulation speed - How long it takes for each turn (in seconds)" << endl << endl;
+			cout << "Example:" << endl;
+			cout << "10" << endl;
+			cout << "5" << endl;
+			cout << "25" << endl;
+			cout << "1.5" << endl;
+			Console::setCursorPosition(26, 0);
+			inFile.close();
+			Sleep(5000);
+			exit(1);
+		}
+	}
+
+
+	// Get position on X Axis
+	int getXPosition() {
+		return position[0];
+	}
+
+	// Get position on Y Axis
+	int getYPosition() {
+		return position[1];
+	}
+
+	// Get width of track
+	int getWidth() {
+		return width;
+	}
+
+	// Get simulation speed
+	int getSpeed() {
+		return speed;
+	}
+};
 
 class AnimalAction
 {
 public:
-	string name, chance;
-	int move;
+	string name;
+	int chance, move;
+
+	string sections[3];
+
+
+	// Default class when no lines are entered
+	AnimalAction() {
+		name = "null";
+	}
 
 	AnimalAction(string entry) {
-		stringstream ss(entry);
-		vector<string> result;
-		
-		string substr, strMove;
-		getline(ss, substr) >> name;
-		getline(ss, substr, ',') >> chance;
-		getline(ss, substr, ',') >> strMove;
-		//while (ss.good())
-		//{
-		//	string substr;
-		//	getline(ss, substr, ',');
-		//	//result.push_back(substr);
-		//	cout << substr;
-		//}
-		cout << name << chance << strMove;
+		char delimiter = ',';
+		string acc = "";
+		int currentSection = 0;
+		for (int i = 0; i < entry.size(); i++)
+		{
+			// Go through each character of the string
+			if (entry[i] == delimiter)
+			{
+				// If the chracter is a comma (,) add the
+				// last set of chracters to a String array and
+				// clear string and proceed to next section index
+				sections[currentSection] = acc;
+				acc = "";
+				currentSection += 1;
+			}
+			else if (entry[i] == '_') {
+
+				// If character is an underscore (_), replace with whitespace.
+				acc += ' ';
+			} else {
+
+				// Add character to set of characters
+				acc += entry[i];
+			}
+
+		}
+
+		// Set name of action to zero-th index of sections
+		name = sections[0];
+
+		// Set chance of action to first index of sections
+		chance = stoi(sections[1]);
+
+		// Set move of action to second index of sections
+		if (sections[2][0] == '-') {
+			// If move is negative, remove minus (-)
+			// from string and turn string to int
+			// and turn int to negative
+			sections[2].erase(sections[2].begin());
+			move = -stoi(sections[2]);
+		}
+		else {
+			// Convert string to int
+			move = stoi(sections[2]);
+		}
 	}
 };
 
 class Animal {
 private:
-	int position;
-	char character;
+	string name;
+	int position = 0;
+	char character = 'A';
 	vector<AnimalAction> actions;
 
 public:
-	Animal(int positionIn, char characterIn, string actionsFilename) {
+	Animal() {}
+
+	Animal(string nameIn, int positionIn, char characterIn, string actionsFilename) {
+
+
+		name = nameIn;
 		position = positionIn;
 		character = characterIn;
 		ifstream inFile(actionsFilename);
 		
-		string line;
-		while (getline(inFile, line))
-		{
-			actions.push_back(AnimalAction(line));
+		// Check if the actions file exists
+		if (inFile) {
+			// Action file exists
+			string line;
+			while (getline(inFile, line))
+			{
+				// Add a new AnimalAction with the line to actions vector
+				actions.push_back(AnimalAction(line));
+			}
+		}
+		else {
+			// Actions file doesn't exist - Throw an error with red text
+			// then quit the program after 5 seconds.
+			Console::setColour(Console::RED, Console::WHITE);
+			Console::setCursorPosition(2, 0);
+			cout << "Error trying to read file." << endl;
+			Console::setCursorPosition(3, 0);
+			cout << "File `" << actionsFilename << "` may not exist" << endl;
+			Console::setCursorPosition(5, 0);
+			Console::setColour(Console::WHITE, Console::BLACK);
+			cout << "`" << actionsFilename << "` requires the following format:" << endl;
+			cout << "Name,Chance,Moves" << endl;
+			cout << "Name - Name of action" << endl;
+			cout << "Chance - Chance out of 100% of happening" << endl;
+			cout << "Moves - Number of moves to go (can be negative or positive)" << endl << endl;
+			cout << "Example:" << endl;
+			cout << "Sleep,5,0" << endl;
+			cout << "Trip,10,-5" << endl;
+			cout << "Run,40,5" << endl;
+			Console::setCursorPosition(26, 0);
+			inFile.close();
+			Sleep(5000);
+			exit(1);
 		}
 
 		inFile.close();
 	}
 
-	void getRandomAction() {
-		int randomPercent = (rand() % 100);
-		for (int i = 0; i < actions.size(); i++) {
-			cout << actions[i].chance;
-			//int possibility = randomPercent / actions[i].chance;
-			//cout << possibility;
+	AnimalAction getRandomAction() {
+		int randomPercent, randomActionIndex;
+		AnimalAction selectedAction;
+		
+		// Generate a random number from 1 to 100
+		randomPercent = (rand() % 100) + 1;
+
+		// Check if the selected action name is null
+		while (selectedAction.name == "null") {
+			// Name is null, thefore empty selected action.
+
+			// Generate random number based on action vector
+			randomActionIndex = (rand() % actions.size());
+
+			// Checks if the random percentage is higher than the action's chance
+			if (randomPercent > actions[randomActionIndex].chance) {
+				// Set selected action to the action where the random
+				// percentage is higher than the action's chance variable.
+				selectedAction = actions[randomActionIndex];
+				break;
+			}
+			else {
+				// Generate a new random percentage and
+				// new AnimalAction with name of null
+				randomPercent = (rand() % 100) + 1;
+				selectedAction = AnimalAction();
+			}
+
 		}
+
+		return selectedAction;
 	}
 
+	// Get name
+	string getName() {
+		return name;
+	}
+
+	// Get position
 	int getPosition() {
 		return position;
 	}
 
-	int getCharacter() {
+
+	// Get character
+	char getCharacter() {
 		return character;
 	}
 
+	// Set position
 	void setPosition(int positionIn) {
 		position = positionIn;
 	}
+
+	// set character
+	void setCharacter(char characterIn) {
+		character = characterIn;
+	}
 };
+
+void drawLineH(int, int, int, char);
+void drawAnimal(int, int, char);
+void drawTrack(int, int, int, Animal, Animal, AnimalAction);
 
 
 int main()
 {
-	const int TRACK_LENGTH = 50;
+	// Set random seed to current time
+	srand(time(0));
+
+	// Old track configuration
+	/*
+	const int TRACK_LENGTH = 25;
 	const int TRACK_X = 10;
 	const int TRACK_Y = 5;
+	const double TRACK_SPEED = 1.5;
+	*/
 
-	Animal hare = Animal(0, 'H', "hare_actions.txt"); // Hare is when turn is 0
-	Animal tortoise = Animal(0, 'T', "tortoise_actions.txt"); // Tortoise is when turn is 1
+	// New track configuration
+	Track track = Track("track_config.txt");
+
+	// Tracks moves for Hare and Tortoise
+	// respectively
+	int moves[2] = { 0, 0 };
+
+	// Create new Animal instances with the following parameters
+	Animal hare = Animal("Hare", 0, 'H', "hare_actions.txt");
+	Animal tortoise = Animal("Tortoise", 0, 'T', "tortoise_actions.txt");
+
+	// Set Console Window title
 	Console::setWindowTitle("Hare and Tortoise");
 
-	srand(static_cast<unsigned int>(time(0)));
+	// Set Game Over to false
+	bool gameOver = false;
 
+	// Set turns to 0
 	int turn = 0;
-	while (tortoise.getPosition() != TRACK_LENGTH || hare.getPosition() != TRACK_LENGTH) {
-		drawLineH(TRACK_X, TRACK_Y, TRACK_LENGTH, '.');
 
+	// Create variables for current animal and other animal
+	Animal currentAnimal, otherAnimal;
+
+	// Check if game is not over
+	while (!gameOver) {
+
+		// Check turn
 		if (!turn) {
-			hare.getRandomAction();
-			//hare.setPosition(hare.getPosition())
+			// If turn is 0
+			// Set current animal to hare
+			// and other animal to tortoise
+			currentAnimal = hare;
+			otherAnimal = tortoise;
+		} else {
+			// If turn is 1
+			// and current animal to tortoise
+			// Set other animal to hare
+			currentAnimal = tortoise;
+			otherAnimal = hare;
+
 		}
 
-		/*if (GetKeyState('T') & 0x8000)
-		{
-			tortoise.setPosition(tortoise.getPosition() + 1);
-			
+		// Get an action from current animal
+		AnimalAction action = currentAnimal.getRandomAction();
 
+		// Checks if the current animal position
+		// + the action moves is less or equal to 0
+		if (currentAnimal.getPosition() + action.move < 0) {
+
+			// Set current animal position to 0
+			currentAnimal.setPosition(0);
+
+			// Checks if the current animal position
+			// + the action moves is more than track width
+		} else if (currentAnimal.getPosition() + action.move > track.getWidth()) {
+
+			// Set current animal position
+			// to track width and end game
+			currentAnimal.setPosition(track.getWidth());
+			gameOver = true;
+		} else {
+
+			// Add action move the the current animal's position
+			currentAnimal.setPosition(currentAnimal.getPosition() + action.move);
 		}
 
-		if (GetKeyState('H') & 0x8000)
-		{
-			hare.setPosition(hare.getPosition() + 1);
+		// Draw the track with both animals, depending on their position
+		drawTrack(track.getXPosition(), track.getYPosition(), track.getWidth(), currentAnimal, otherAnimal, action);
 
-		}*/
+		// Resets cursor position to 0,0
+		Console::setCursorPosition(0, 0);
 
+		// Old speed method
+		//Sleep(1500);
 
-
-		if (tortoise.getPosition() != hare.getPosition()) {
-			drawAnimal(TRACK_X + tortoise.getPosition(), TRACK_Y, tortoise.getCharacter());
-			drawAnimal(TRACK_X + hare.getPosition(), TRACK_Y, hare.getCharacter());
-			drawLineH(TRACK_X, TRACK_Y + 1, TRACK_LENGTH, ' ');
+		// New speed method
+		// Checks if track speed is 0
+		if (!track.getSpeed()) {
+			// Disable delay between each turn
+			Sleep(0);
 		}
 		else {
-			drawAnimal(TRACK_X + hare.getPosition(), TRACK_Y, 'B');
-			Console::setCursorPosition(TRACK_Y + 1, TRACK_X);
-			cout << "The Hare and Tortoise are in the same position.";
+			// Wait number of seconds before next turn
+			// depending on track config
+			Sleep(1000 * track.getSpeed());
 		}
 
-		Sleep(200);
+		// Applies currentAnimal and otherAnimal
+		// instances back to their original instances
+		// depending on the turn
+		if (!turn) {
+			hare = currentAnimal;
+			tortoise = otherAnimal;
+			
+			// Add 1 move to hare
+			moves[0] += 1;
+		}
+		else {
+			tortoise = currentAnimal;
+			hare = otherAnimal;
+
+			// Add 1 move to tortoise
+			moves[1] += 1;
+		}
+
+		// Set turn to opposite of current turn
+		// e.g, 1 = 0, 0 = 1
+		turn = !turn;
 	}
 
 	// Output game result
+	ofstream gameResult("result.txt");
 
+	// Checks if currentAnimal's name is Hare
+	// If so, set winnerMoves to Hare's moves
+	// otherwise, tortoise's moves
+	int winnerMoves = currentAnimal.getName() == "Hare" ? moves[0] : moves[1];
+
+	// Output game result to file
+	// and close file
+	gameResult << "The winner is the " << currentAnimal.getName() << ", it took " << winnerMoves << " moves to win.";
+	gameResult.close();
+
+
+	// Set cursor further down in
+	// console and end the program
+	Console::setCursorPosition(30, 0);
 	return 0;
 }
 
 void drawLineH(int x, int y, int length, char character) {
+
+	// Set console cursor position
+	// to x and y and run a for loop
+	// until it reaches length, to 
+	// output specified character
 	Console::setCursorPosition(y, x);
 	for (int i = 0; i < length; i++) {
 		cout << character;
@@ -146,8 +439,50 @@ void drawLineH(int x, int y, int length, char character) {
 }
 
 void drawAnimal(int x, int y, char character) {
+	// Output character at specific x and y
 	Console::setCursorPosition(y, x);
 	cout << character;
 }
 
+void drawTrack(int x, int y, int length, Animal current, Animal other, AnimalAction action) {
 
+	// Reset base track
+	drawLineH(x, y, length, '.');
+	drawLineH(x, y + 1, length * 2, ' ');
+	drawLineH(x, y + 2, length * 2, ' ');
+
+	// Checks if both animals are
+	// in the same location
+	if (current.getPosition() != other.getPosition()) {
+		// Current animal and Other animal
+		// are not in the same location
+
+		// Draw animal's character at animal's
+		// location
+		drawAnimal(x + current.getPosition(), y, current.getCharacter());
+		drawAnimal(x + other.getPosition(), y, other.getCharacter());
+
+		// Set console cursor to x and y + 1
+		// and output action of current animal
+		Console::setCursorPosition(y + 1, x);
+		cout << "The " << current.getName() << " used " << action.name << ", moving them " << action.move << " spaces";
+	}
+	else {
+		// Current animal and Other animal
+		// are in the same location
+
+		// Draw 'B' (for both animals) at either
+		// current or other's position
+		drawAnimal(x + current.getPosition(), y, 'B');
+
+		// Set console cursor position to x, y + 1
+		// and output that both animals are in the same location.
+		Console::setCursorPosition(y + 1, x);
+		cout << "The Hare and Tortoise are in the same position.";
+
+		// Set console cursor position to x, y + 2
+		// and output action of current animal
+		Console::setCursorPosition(y + 2, x);
+		cout << "The " << current.getName() << " used " << action.name << ", moving them " << action.move << " spaces";
+	}
+}
